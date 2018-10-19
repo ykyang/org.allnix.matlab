595ba015c4f58dd96c083b8c014907d31976f848
Yi-Kun Yang <ykyang@gmail.com>
2018-10-19 09:31:32 -0500

    Learn sprintf formatting

--- test/+allnix/BasicTest.m
+++ test/+allnix/BasicTest.m
@@ -10,15 +10,37 @@ classdef BasicTest < matlab.unittest.TestCase
     end
     
     methods (Test)
-        function testString(me)
+        function testConcatenation(me)
+                        
             % horizontal array concatenation, [s1, s2, ..., sN]
             % preserves tailing white spaces
             s = ['a ', 'b ', 'c'];
             me.assertEqual(s, 'a b c');
-            
+           
             % strcat removes tailing white spaces
             s = strcat(' a ', ' b ', ' c ');
             me.assertEqual(s, ' a b c');
+            
+            % not relizable
+            s = ['a', 1, 'b', 2];
+            
+            % old, faithful sprintf
+            s = sprintf('%s %s %s', 'a', 'b', 'c');
+            me.assertEqual(s, 'a b c');
+
+            s = sprintf('%s %i %g', 'a', 2, 13.5);
+            me.assertEqual(s, 'a 2 13.5');
+            
+            s = sprintf('%s %i %g', 'a', 2, 13.1415967);
+            me.assertEqual(s, 'a 2 13.1416');
+            
+            s = sprintf('%s %g %.16g', 'a', 2, 13.1415967);
+            me.logger.info('s = %s', s);
+            me.assertEqual(s, 'a 2 13.1415967');
+            
+            s = sprintf('%s %g %.16G', 'a', 2, 13.1415967);
+            me.logger.info('s = %s', s);
+            me.assertEqual(s, 'a 2 13.1415967');
         end
     end
 end
