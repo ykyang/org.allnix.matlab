c64739cf530fa50f2b9ee75c1651520785855244
Yi-Kun Yang <ykyang@gmail.com>
2018-10-26 18:08:02 -0500

    Learn timer

--- learn.m
+++ learn.m
@@ -36,3 +36,38 @@ r = rand(n,1);
 %disp(class(r))
 %fprintf(2, '%s\n',class(r));
 %plot(r)
+
+
+% t = timer('TimerFcn', {@kernel, "1"});
+% startat(t, now+0.1/86400);
+% disp('blocking?');
+% t1 = timer('TimerFcn', {@kernel, "2"});
+% startat(t1, now+0.1/86400);
+%wait([t, t1])
+
+t = timer;
+t.TimerFcn = {@kernel, "2"};
+t.ExecutionMode = 'fixedSpacing';
+t.Name = 'T';
+
+disp("timeer name: " + t.Name);
+start(t);
+
+
+
+
+
+function kernel(tim, evt, id)
+a = 0;
+b = 1.5;
+c = 3.6;
+tic
+disp("start " + id);
+%pause(5);
+ for i = 1: 500000000
+     a = a + i*b + c;
+ end
+disp("stop " + id);
+toc
+disp(a);
+end
