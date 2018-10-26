9ce7c8df618f60e3543b9cac2a303d06e529a48b
Yi-Kun Yang <ykyang@gmail.com>
2018-10-26 09:46:22 -0500

    doc

--- AllnixMain.m
+++ AllnixMain.m
@@ -1,3 +1,13 @@
+%Learn to use external Java libraries
+%     
+%   1. Use gradle to package all Java libraries
+%   2. cp to lib/
+%   3. use add_javapath to load dynamically
+%   4. test in AllnixMain
+%   5. package up with compiler, add main/ and lib/ to
+%      included list
+
+
 import java.util.HashMap
 import java.io.File
 import com.fasterxml.jackson.databind.ObjectMapper
