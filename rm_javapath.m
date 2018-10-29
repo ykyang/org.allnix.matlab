6957c9f840f9315b089be45e1d6a78479a7c313f
Yi-Kun Yang <ykyang@gmail.com>
2018-10-28 21:08:12 -0500

    Add script to load Java libraries

--- rm_javapath.m
+++ rm_javapath.m
@@ -5,5 +5,5 @@ list = [list; dir(...
     "/home/ykyang/work/org.allnix.java/hf/"...
     + "build/distributions/hf-0.0.1b1/lib/*.jar")];
 
-fcn = @(x) javaaddpath(fullfile(x.folder, x.name));
+fcn = @(x) javarmpath(fullfile(x.folder, x.name));
 arrayfun(fcn, list);
